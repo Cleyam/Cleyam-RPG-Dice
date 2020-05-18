87a1be48ce5e3adcd787bb30cb515875337a632c
Cleyam <cleyam@gmail.com>
2020-05-18 15:28:22 +0200

    Base + integration bootstrap/jquery

---
+++ cleyam-rpg-dice.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Plugin Name: Cleyam-RPG-Dice
+ * Description: A plugin used to simulate dice rolls for roleplay games. shortcode [cleyamDice] where you want to use it !
+ * Version: 1.0
+ * Author: Cleyam ( Thomas Hermant )
+ * Author URI: https://github.com/Cleyam
+ * 
+ */
+
+//  Pour hook dans la page wordpress
+// add_action('HOOK', 'cleyam_dice');
+
+// Pour gerer événements WP ( ex : template par défaut pour article )
+// add_fitler('FILTER', 'cleyam_dice');
+
+// Add the custom js file into the plugin, compatible with jquery
+wp_enqueue_script('cleyam-rpg-dice', plugin_dir_url(__FILE__) . 'js/cleyam-rpg-dice.js', array('jquery'), null, true);
+wp_register_style('prefix_bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
+wp_enqueue_style('prefix_bootstrap');
+
+function cleyamDice()
+{
+    ob_start();
+    require_once('form/form.html');
+    $html = ob_get_clean();
+    return $html;
+}
+add_shortcode('cleyamDice', 'cleyamDice');
---
+++ js/cleyam-rpg-dice.js
@@ -0,0 +1,12 @@
+(function ($) {
+    $(document).ready(function () {
+
+        // Test
+        $('#roll').on('click', function () {
+            alert('coucou');
+        })
+
+
+
+    });
+}(jQuery));
