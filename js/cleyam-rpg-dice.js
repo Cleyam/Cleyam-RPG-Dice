abdb6bbbc16d59fc90d8fbc645549d9c989d83db
Cleyam <cleyam@gmail.com>
2020-05-19 13:51:38 +0200

    fix result

--- js/cleyam-rpg-dice.js
+++ js/cleyam-rpg-dice.js
@@ -7,7 +7,7 @@
             let type = $('#diceType').val();
             let results = [];
             for (let loop = 0; loop < number; loop++) {
-                results.push("<button class='btn text-light mx-2'>" + Math.floor(Math.random() * (type - 1 + 1)) + 1 + "</button>")
+                results.push("<button class='btn text-light mx-2'>" + (Math.floor(Math.random() * (type - 1 + 1)) + 1) + "</button>")
             }
             $('#result').html(results);
         })
